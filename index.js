9ef2af768a1c4c6c87da0e0799892fca353215f9
Rafael Basso <44483784+RafaelBassoDev@users.noreply.github.com>
2021-11-25 20:36:21 -0300

    Removed unused code from index.js

--- index.js
+++ index.js
@@ -12,28 +12,4 @@ app.use(express.json())
 
 app.use(routers)
 
-// app.get("/src/index.html", function(req, res){
-//     res.sendFile(__dirname + "/src/index.html")
-// })
-
-// app.get("/src/read-more.html", function(req, res){
-//     res.sendFile(__dirname + "/src/read-more.html")
-// })
-
-// app.get("/src/characters.html", function(req, res){
-//     res.sendFile(__dirname + "/src/characters.html")
-// })
-
-// app.get("/src/game.html", function(req, res){
-//     res.sendFile(__dirname + "/src/game.html")
-// })
-
-// app.get("/src/login.html", function(req, res){
-//     res.sendFile(__dirname + "/src/login.html")
-// })
-
-// app.get("/src/register.html", function(req, res){
-//     res.sendFile(__dirname + "/src/register.html")
-// })
-
 app.listen(8080)
